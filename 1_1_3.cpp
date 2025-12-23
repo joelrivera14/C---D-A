0b1b73550fb91faa17528067fe7486705e724944
joel rivera <jorivera114@gmail.com>
2025-12-23 16:15:33 -0500

    pointer play

--- 1_1_3.cpp
+++ 1_1_3.cpp
@@ -2,7 +2,14 @@
 
 int main()
 {
+    // messing with pointers
+    int x = 10;
+    int u = 20;
+    int *r = &u;
+    int *p = &x;
+    std::cout << p << " " << *p << std::endl;
 
-    std::cout << "Testing" << std::endl;
+    *p = *r;
+    std::cout << p << " " << *p << std::endl;
     return 0;
 }
