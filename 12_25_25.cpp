1688cbd7e8c8daac79f902cc452287623b94e351
joel rivera <jorivera114@gmail.com>
2025-12-25 17:11:25 -0500

    f

--- 12_25_25.cpp
+++ 12_25_25.cpp
@@ -7,8 +7,32 @@ int main(){
     Explicit casting*/
     double d = 3.999;
     int a = static_cast<int>(d);
-    
     std::cout<<(a)<<std::endl;
+
+    //Implicit Casting
+    double t = 2.3456;
+    int b = t;
+    std::cout<<(b)<<std::endl;
+
+    //switch statements
+    int x;
+    std::cout<<"Enter a number: ";
+    std::cin>>x;
+    switch(x){
+        case 1:
+        std::cout<<"pelp"<<std::endl;
+        break;
+        default:
+        std::cout<<"meh"<<std::endl;
+        break;
+    }
+
+    //for loops
+    const int num = 100;
+    double be[num]={};
+    for(int i = 0; i<num;++i){
+        std::cout<<be[i]<<" ";
+    }
 
     return 0;
 }
