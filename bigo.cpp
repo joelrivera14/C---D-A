91e17cb2800dc67d120517abffa03ce8b7ce9487
joel rivera <jorivera114@gmail.com>
2026-01-13 16:00:18 -0500

    recursion

---
+++ bigo.cpp
@@ -0,0 +1,17 @@
+/* Big O describes the computational complexity of an algorithm
+computational complexity is composed of two parts, the first being time complexity which measures
+the amount of time the algorithm needs to run relative to input size and
+space complexity which is the amount of memory used by the algorithm relative to input size
+
+Time complexity: as the input size grows, how much longer does the algorithm take to complete?
+-describe bow the number of operations changes as the input changes
+Space complexity: as the input size grows, how much more memory does the algorithm use?
+
+O(1): best complexity possible, means that the algorithm always
+      uses the same amount of resources rgardless of input
+
+O(log n): extremely fast, common time complexity is O(n * log n), at each step we
+          are reducing our search space by 50% which gives the logarithmic time complexity
+
+
+*/
