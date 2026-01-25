445e2f5056616744f6139d04ee62b59e340cdb0a
joel rivera <jorivera114@gmail.com>
2026-01-25 12:34:16 -0500

    vectors

--- vector.cpp
+++ vector.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <vector>
 
+void print(std::vector<int>* v);
 int main()
 {
     std::vector<int> *i = new std::vector<int>{4,5,6,7,2};
     std::cout<<(*i)[0]<<"\n";
-    ++i;
-    std::cout<<(*i)[0]<<"\n";
+    print(i);
     delete i;
     return 0;
+}
+void print(std::vector<int>* v){
+    for(int i = 0; i<v->size(); ++i){
+       std::cout<<(*v)[i]<<' '; 
+    }
+    std::cout<<"\n";
 }
