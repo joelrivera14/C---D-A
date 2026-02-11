670f0fb9ebbe801b7659e20a6abdf098e96f3db2
joel rivera <jorivera114@gmail.com>
2026-02-11 10:35:40 -0500

    swtichclass

--- switchStatement.cpp
+++ switchStatement.cpp
@@ -5,29 +5,29 @@ int main()
 {
     int day;
     cout << "select dayoff" << "\n";
-    cout << "1" << "\n";
-    cout << "2" << "\n";
-    cout << "3" << "\n";
-    cout << "4" << "\n";
-    cout << "5" << "\n";
+    cout << "1 monday" << "\n";
+    cout << "2 tuesday" << "\n";
+    cout << "3 wednesday" << "\n";
+    cout << "4 thursday" << "\n";
+    cout << "5 friday" << "\n";
     cin >> day;
 
     switch (day)
     {
     case 1:
-        cout << "you said 1";
+        cout << "you work monday" << "\n";
         break;
     case 2:
-        cout << "you said 1";
+        cout << "you work tuesday" << "\n";
         break;
     case 3:
-        cout << "you said 1";
+        cout << "you work wednesday" << "\n";
         break;
     case 4:
-        cout << "you said 1";
+        cout << "you work thursday" << "\n";
         break;
     case 5:
-        cout << "you said 1";
+        cout << "you work friday" << "\n";
         break;
     default:
         break;
