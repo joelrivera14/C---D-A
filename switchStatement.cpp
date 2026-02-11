32d999606d5067b35709b8793f19357b43f2c00a
joel rivera <jorivera114@gmail.com>
2026-02-11 10:31:24 -0500

    switchclass

---
+++ switchStatement.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int day;
+    cout << "select dayoff" << "\n";
+    cout << "1" << "\n";
+    cout << "2" << "\n";
+    cout << "3" << "\n";
+    cout << "4" << "\n";
+    cout << "5" << "\n";
+    cin >> day;
+
+    switch (day)
+    {
+    case 1:
+        cout << "you said 1";
+        break;
+    case 2:
+        cout << "you said 1";
+        break;
+    case 3:
+        cout << "you said 1";
+        break;
+    case 4:
+        cout << "you said 1";
+        break;
+    case 5:
+        cout << "you said 1";
+        break;
+    default:
+        break;
+    }
+    return 0;
+}
