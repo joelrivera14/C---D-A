7802ee52c3ee32ab5ed010b0a25468494e7dc315
joel rivera <jorivera114@gmail.com>
2026-02-12 18:55:02 -0500

    trees

---
+++ trees.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+/* trees: nonlinear data structures in computing
+ relationships in a tree are hierarchical, with some objects being above and
+ some objects being below each other
+ A tree is an abstact data type that stores elments heirarchically
+ with the exception of the topmost element, the root, each element has a parent element
+ and 0 or more children elements
+ a node is a sibling if it shares a parent node with another node
+ a node is external is it has no children, it is internal if it has one or more children
+ external nodes are also known as leaves
+ an edge in a tree is a pair of node such that node u is a parent to node v
+ or vice versa
+ a path is a sequence such that two consecutive nodes form an edge*/
+
+int main()
+{
+
+    return 0;
+}
