c8636928b8aaa9f87928048fd609b330a7b9ee22
joel rivera <jorivera114@gmail.com>
2026-02-14 15:32:27 -0500

    maps

--- maps2.cpp
+++ maps2.cpp
@@ -2,4 +2,11 @@
 
 struct Node
 {
+    Entry entry;
+    Node *parent;
+    Node *left;
+    Node *right;
+
+    Node(const Entry &e)
+        : entry(e), parent(nullptr), left(nullptr), right(nullptr) {}
 };
