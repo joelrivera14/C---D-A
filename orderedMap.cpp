729dde2e4d9432de10095d4f69c4fecd4801311f
joel rivera <jorivera114@gmail.com>
2026-02-25 10:25:33 -0500

    ordered maps

---
+++ orderedMap.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <map>
+
+/*ordered maps:
+    an order relation is kept for the keys of the map
+    we can use a comparator to provide an order relation among keys
+    allows us to define the ordered map relative to the comparator
+    can be provided to the ordered map as an argument to its constructor
+    in order to indicate that an object is not present, the class provides
+    a special sentinel iterator called end
+    */
+
+int main()
+{
+
+    return 0;
+}
