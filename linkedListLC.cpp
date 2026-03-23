a57b5ca5df71311a1b0daa08955d478fea930907
joel rivera <jorivera114@gmail.com>
2026-03-23 10:27:57 -0400

    traversing through the list

--- linkedListLC.cpp
+++ linkedListLC.cpp
@@ -1,24 +1,38 @@
 #include <iostream>
 
+// define this outside the main function so that its not limited to main scope
+// and its globally accessible
+struct LinkedListNode
+{
+    int val;
+    LinkedListNode *next;
+    LinkedListNode(int v) : val(v), next(nullptr) {}
+};
+
+int addSum(LinkedListNode *head);
 int main()
 {
-    struct LinkedListNode
-    {
-        int val;
-        LinkedListNode *next;
-        LinkedListNode(int v) : val(v), next(nullptr) {}
-    };
 
     LinkedListNode *one = new LinkedListNode(1);
     LinkedListNode *two = new LinkedListNode(2);
     LinkedListNode *three = new LinkedListNode(3);
     one->next = two;
     two->next = three;
-    LinkedListNode* head = one;
+    LinkedListNode *head = one;
 
     std::cout << head->val << std::endl;
     std::cout << head->next->val << std::endl;
     std::cout << two->next->val << std::endl;
+    //adding the sum function
+    std::cout<<addSum(head)<<std::endl;
 
     return 0;
+}
+int addSum(LinkedListNode *head) {
+    int sum = 0;
+    while(head != nullptr){
+        sum += head->val;
+        head = head->next;
+    }
+    return sum;
 }
