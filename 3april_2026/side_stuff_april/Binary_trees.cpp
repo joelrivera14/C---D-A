0e4ad5f9c6f8316dee904045ca03930aab3e5a80
joel rivera <jorivera114@gmail.com>
2026-04-09 13:56:11 -0400

    preorder, inorder & postorder dfs

--- 3april_2026/side_stuff_april/Binary_trees.cpp
+++ 3april_2026/side_stuff_april/Binary_trees.cpp
@@ -8,6 +8,53 @@ struct TreeNode
     TreeNode(int v) : val(v), left(nullptr), right(nullptr) {}
 };
 
+void dfs(TreeNode *node)
+{
+    if (node == nullptr)
+    {
+        return;
+    }
+    dfs(node->left);
+    dfs(node->right);
+    return;
+}
+
+void preorderDfs(TreeNode *node)
+{
+    if (node == nullptr)
+    {
+        return;
+    }
+    std::cout << node->val << "\n";
+    preorderDfs(node->left);
+    preorderDfs(node->right);
+    return;
+}
+
+void inorderDfs(TreeNode *node)
+{
+    if (node == nullptr)
+    {
+        return;
+    }
+    inorderDfs(node->left);
+    std::cout << node->val << "\n";
+    inorderDfs(node->right);
+    return;
+}
+
+void postorderDfs(TreeNode *node)
+{
+    if (node == nullptr)
+    {
+        return;
+    }
+    postorderDfs(node->left);
+    postorderDfs(node->right);
+    std::cout << node->val << "\n";
+    return;
+}
+
 int main()
 {
     return 0;
