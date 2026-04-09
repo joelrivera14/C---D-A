8334eee0122a8fcea8b559adaf40e3ebd6847acd
joel rivera <jorivera114@gmail.com>
2026-04-09 12:39:43 -0400

    binary tree

---
+++ 3april_2026/side_stuff_april/Binary_trees.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int v) : val(v), left(nullptr), right(nullptr) {}
+};
+
+int main()
+{
+    return 0;
+}
